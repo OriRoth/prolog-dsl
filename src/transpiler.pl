ecf8418fbea787216d6e0b47b43eab83817e5170
OriRoth <ori.rothh@gmail.com>
2023-03-17 17:36:56 +0200

    complete transpilation of new syntax

--- src/transpiler.pl
+++ src/transpiler.pl
@@ -25,8 +25,14 @@ temp_name(Temp_Number, Temp_Name) :-
  * Program *
  ***********/
 
+get_function_names([], []).
+get_function_names([definition(function_id(Name), _, _) | Ds], Out) :-
+	get_function_names(Ds, Out1),
+	append([Name], Out1, Out).
+
 transpile_program(program(Definitions), Out) :-
-	transpile_definitions(Definitions, Definitions_Out),
+	get_function_names(Definitions, Function_Names),
+	transpile_definitions(Definitions, Function_Names, Definitions_Out),
 	join(Definitions_Out, "\n", Out).
 
 transpile_to_prolog(W, Out) :-
@@ -38,13 +44,11 @@ transpile_to_prolog(W, Out) :-
  * Definitions *
  ***************/
 
-transpile_definitions([], []).
-transpile_definitions([D | Ds], [Out1 | Out2]) :-
-	transpile_definition(D, Out1),
-	transpile_definitions(Ds, Out2).
+transpile_definitions([], _, []).
+transpile_definitions([D | Ds], Function_Names, [Out1 | Out2]) :-
+	transpile_definition(D, Function_Names, Out1),
+	transpile_definitions(Ds, Function_Names, Out2).
 
-transpile_variable(in, "Var_in") :- !.
-transpile_variable(out, "Var_out") :- !.
 transpile_variable(function_id(I), I).
 
 transpile_variables([V], Out) :-
@@ -55,25 +59,33 @@ transpile_variables([V | Vs], Out) :-
 	transpile_variables(Vs, Out3),
 	string_concat(Out2, Out3, Out).
 
-transpile_definition(definition(function_id(Name), Body), Out) :-
+transpile_definition(definition(function_id(Name), Inputs, Body), Function_Names, Out) :-
 	function_name(Name, Function_Name),
-	string_concat(Function_Name, "(Var_in,Var_out):-", Out1),
-	transpile_body(Body, Out2),
-	string_concat(Out1, Out2, Out).
-transpile_definition(definition(function_id(Name), Inputs, Outputs, Body), Out) :-
-	function_name(Name, Function_Name),
-	string_concat(Function_Name, "(", Out1),
-	transpile_variables(Inputs, Out2),
-	string_concat(Out1, Out2, Out3),
-	string_concat(Out3, ",(", Out4),
-	transpile_variables(Outputs, Out5),
-	string_concat(Out4, Out5, Out6),
-	string_concat(Out6, ")):-", Out7),
-	transpile_body(Body, Out8),
-	string_concat(Out7, Out8, Out).
+	string_concat("apply_fun(", Function_Name, Out1),
+	string_concat(Out1, ",", Out2),
+	transpile_variables(Inputs, Out3),
+	string_concat(Out2, Out3, Out4),
+	string_concat(Out4, ",(", Out5),
+	transpile_body(Body, Function_Names, Body_Out, Return_Vars),
+	join(Return_Vars, ",", Out6),
+	string_concat(Out5, Out6, Out7),
+	string_concat(Out7, "))", Out8),
+	(Body_Out = "." ->
+		Out9 = Out8
+		;
+		string_concat(Out8, ":-", Out9)
+	),
+	string_concat(Out9, Body_Out, Out).
 
-transpile_body(Statements, Out) :-
-	transpile_statements(Statements, 1, Out1),
+transpile_body(Statements, Function_Names, Out, Return_Vars) :-
+	is_list(Statements), !,
+	transpile_statements(Statements, Function_Names, 1, Out1, Return_Vars),
+	join(Out1, ",", Out2),
+	string_concat(Out2, ".", Out).
+
+transpile_body(Expression, Function_Names, Out, Return_Vars) :-
+	\+ is_list(Expression), !,
+	transpile_statement(return(Expression), Function_Names, 1, Out1, _, Return_Vars),
 	join(Out1, ",", Out2),
 	string_concat(Out2, ".", Out).
 
@@ -81,86 +93,97 @@ transpile_body(Statements, Out) :-
  * Statements *
  **************/
 
-transpile_statements([], _, []).
-transpile_statements([S | Ss], Temp_Counter, Out) :-
-	transpile_statement(S, Temp_Counter, Out1, Temp_Counter1),
-	transpile_statements(Ss, Temp_Counter1, Out2),
-	append(Out1, Out2, Out).
+transpile_statements([], _, _, [], []).
+transpile_statements([S | Ss], Function_Names, Temp_Counter, Out, Return_Vars) :-
+	transpile_statement(S, Function_Names, Temp_Counter, Out1, Temp_Counter1, Return_Vars1),
+	transpile_statements(Ss, Function_Names, Temp_Counter1, Out2, Return_Vars2),
+	append(Out1, Out2, Out),
+	append(Return_Vars1, Return_Vars2, Return_Vars).
 
-transpile_statement(prolog_id(I), Temp_Counter, [I], Temp_Counter).
-transpile_statement(assignment(Vs, X), Temp_Counter, Out, Temp_Counter_Out) :-
+transpile_function_id_or_variable(I, Function_Names, I) :-
+	\+ member(I, Function_Names), !.
+transpile_function_id_or_variable(I, Function_Names, Out) :-
+	member(I, Function_Names), !,
+	function_name(I, Out).
+
+transpile_statement(imperative(prolog_id(I)), _, Temp_Counter, [I], Temp_Counter, []).
+transpile_statement(imperative(assignment(Vs, X)), Function_Names, Temp_Counter, Out, Temp_Counter_Out, []) :-
 	transpile_variables(Vs, Out1),
 	string_concat("(", Out1, Out2),
 	string_concat(Out2, ")=", Out3),
-	transpile_expression(X, Temp_Counter, Statements_Out, Expression_Out, Temp_Counter_Out),
+	transpile_expression(X, Function_Names, Temp_Counter, Statements_Out, Expression_Out, Temp_Counter_Out),
 	string_concat(Out3, Expression_Out, Out4),
 	append(Statements_Out, [Out4], Out).
-transpile_statement(invocation(prolog_id(I), Arguments), Temp_Counter, Out, Temp_Counter_Out) :-
+transpile_statement(imperative(invocation(prolog_id(I), Arguments)), Function_Names, Temp_Counter, Out, Temp_Counter_Out, []) :-
 	string_concat(I, "(", Out1),
-	transpile_expressions(Arguments, Temp_Counter, Statements_Out, Arguments_Out, Temp_Counter_Out),
+	transpile_expressions(Arguments, Function_Names, Temp_Counter, Statements_Out, Arguments_Out, Temp_Counter_Out),
 	join(Arguments_Out, ",", Out2),
 	string_concat(Out1, Out2, Out3),
 	string_concat(Out3, ")", Out4),
 	append(Statements_Out, [Out4], Out).
-transpile_statement(invocation(function_id(I), Arguments), Temp_Counter, Out, Temp_Counter_Out) :-
-	function_name(I, Out1),
-	string_concat(Out1, "(", Out2),
-	transpile_expressions(Arguments, Temp_Counter, Statements_Out, Arguments_Out1, Temp_Counter_Out),
-	string_concat(Arguments_Out1, "_", Arguments_Out),
-	join(Arguments_Out, ",", Out3),
-	string_concat(Out2, Out3, Out4),
-	string_concat(Out4, ")", Out5),
-	append(Statements_Out, [Out5], Out).
+transpile_statement(imperative(invocation(function_id(I), Arguments)), Function_Names, Temp_Counter, Out, Temp_Counter_Out, []) :-
+	transpile_function_id_or_variable(I, Function_Names, Out1),
+	string_concat("apply_fun(", Out1, Out2),
+	string_concat(Out2, ",", Out3),
+	transpile_expressions(Arguments, Function_Names, Temp_Counter, Statements_Out, Arguments_Out, Temp_Counter_Out),
+	join(Arguments_Out, ",", Out4),
+	string_concat(Out3, Out4, Out5),
+	string_concat(Out5, ",_)", Out6),
+	append(Statements_Out, [Out6], Out).
+
+transpile_statement(return(Expression), Function_Names, Temp_Counter, Statements_Out, Temp_Counter_Out, [Expression_Out]) :-
+	transpile_expression(Expression, Function_Names, Temp_Counter, Statements_Out, Expression_Out, Temp_Counter_Out).
 
 /***************
  * Expressions *
  ***************/
 
-transpile_expressions([], Temp_Counter, [], [], Temp_Counter).
-transpile_expressions([X | Xs], Temp_Counter, Statements_Out, [X_Out | Xs_Out], Temp_Counter_Out) :-
-	transpile_expression(X, Temp_Counter, Statements_Out1, X_Out, Temp_Counter1),
-	transpile_expressions(Xs, Temp_Counter1, Statements_Out2, Xs_Out, Temp_Counter_Out),
+transpile_expressions([], _, Temp_Counter, [], [], Temp_Counter).
+transpile_expressions([X | Xs], Function_Names, Temp_Counter, Statements_Out, [X_Out | Xs_Out], Temp_Counter_Out) :-
+	transpile_expression(X, Function_Names, Temp_Counter, Statements_Out1, X_Out, Temp_Counter1),
+	transpile_expressions(Xs, Function_Names, Temp_Counter1, Statements_Out2, Xs_Out, Temp_Counter_Out),
 	append(Statements_Out1, Statements_Out2, Statements_Out).
 
 /**********************
  * Atomic Expressions *
  **********************/
 
-transpile_expression(number(N), Temp_Counter, [], N_String, Temp_Counter) :-
+transpile_expression(number(N), _, Temp_Counter, [], N_String, Temp_Counter) :-
 	number_string(N, N_String).
-transpile_expression(string(S), Temp_Counter, [], Out, Temp_Counter) :-
+transpile_expression(string(S), _, Temp_Counter, [], Out, Temp_Counter) :-
 	string_concat("\"", S, Out1),
 	string_concat(Out1, "\"", Out).
-transpile_expression(in, Temp_Counter, [], "Var_in", Temp_Counter).
-transpile_expression(out, Temp_Counter, [], "Var_out", Temp_Counter).
-transpile_expression(prolog_id(I), Temp_Counter, [], I, Temp_Counter).
-transpile_expression(function_id(I), Temp_Counter, [], I, Temp_Counter).
+transpile_expression(prolog_id(I), _, Temp_Counter, [], I, Temp_Counter).
+transpile_expression(function_id(I), Function_Names, Temp_Counter, [], Out, Temp_Counter) :-
+	transpile_function_id_or_variable(I, Function_Names, Out).
 
 /************************
  * Compound Expressions *
  ************************/
 
-transpile_expression(parenthesized(X), Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
-	transpile_expression(X, Temp_Counter, Statements_Out, Out, Temp_Counter_Out).
-transpile_expression(tuple(Xs), Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
-	transpile_expressions(Xs, Temp_Counter, Statements_Out, Xs_Out, Temp_Counter_Out),
+transpile_expression(parenthesized(X), Function_Names, Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
+	transpile_expression(X, Function_Names, Temp_Counter, Statements_Out, Out, Temp_Counter_Out).
+transpile_expression(tuple(Xs), Function_Names, Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
+	transpile_expressions(Xs, Function_Names, Temp_Counter, Statements_Out, Xs_Out, Temp_Counter_Out),
 	join(Xs_Out, ",", Out1),
 	string_concat("(", Out1, Out2),
 	string_concat(Out2, ")", Out).
-transpile_expression(invocation(prolog_id(I), Xs), Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
-	transpile_expressions(Xs, Temp_Counter, Statements_Out, Xs_Out, Temp_Counter_Out),
+transpile_expression(invocation(prolog_id(I), Xs), Function_Names, Temp_Counter, Statements_Out, Out, Temp_Counter_Out) :-
+	transpile_expressions(Xs, Function_Names, Temp_Counter, Statements_Out, Xs_Out, Temp_Counter_Out),
 	string_concat(I, "(", Out1),
 	join(Xs_Out, ",", Out2),
 	string_concat(Out1, Out2, Out3),
 	string_concat(Out3, ")", Out).
-transpile_expression(invocation(function_id(I), Xs), Temp_Counter, Statements_Out, Temp_Name, Temp_Counter_Out) :-
-	transpile_expressions(Xs, Temp_Counter, Statements1, Xs_Out1, Temp_Counter1),
+transpile_expression(invocation(function_id(I), Xs), Function_Names, Temp_Counter, Statements_Out, Temp_Name, Temp_Counter_Out) :-
+	transpile_function_id_or_variable(I, Function_Names, Function_Name),
+	string_concat("apply_fun(", Function_Name, Statement1),
+	string_concat(Statement1, ",", Statement2),
+	transpile_expressions(Xs, Function_Names, Temp_Counter, Statements1, Xs_Out, Temp_Counter1),
+	join(Xs_Out, ",", Statement3),
+	string_concat(Statement2, Statement3, Statement4),
+	string_concat(Statement4, ",", Statement5),
 	temp_name(Temp_Counter1, Temp_Name),
 	Temp_Counter_Out is Temp_Counter1 + 1,
-	append(Xs_Out1, [Temp_Name], Xs_Out),
-	function_name(I, Function_Name),
-	string_concat(Function_Name, "(", Statement1),
-	join(Xs_Out, ",", Statement2),
-	string_concat(Statement1, Statement2, Statement3),
-	string_concat(Statement3, ")", Statement),
+	string_concat(Statement5, Temp_Name, Statement6),
+	string_concat(Statement6, ")", Statement),
 	append(Statements1, [Statement], Statements_Out).
--- src/transpiler_test.pl
+++ src/transpiler_test.pl
@@ -2,23 +2,28 @@
 :- use_module(transpiler).
 
 compile_prolog(Prolog_Program) :-
+	string_concat(":- multifile apply_fun/2.\n:- multifile apply_fun/3.\n:- multifile apply_fun/4.\n", Prolog_Program, Prolog_Program1),
 	tmp_file_stream(text, File, Stream),
-	write(Stream, Prolog_Program),
+	write(Stream, Prolog_Program1),
 	close(Stream),
 	consult(File).
 
 test(simple_program) :-
-	transpile_to_prolog("Id :- $ = #.", Prolog_Program),
+	transpile_to_prolog("Id(X) = X.", Prolog_Program),
 	compile_prolog(Prolog_Program),
-	fun_Id(1, 1),
-	\+ fun_Id(1, 2).
+	apply_fun(fun_Id, 1, 1),
+	\+ apply_fun(fun_Id, 1, 2).
 
 test(peano) :-
-	transpile_to_prolog("Inc :- $ = s(#). Plus N1, N2 -> N3 :- N1 = 0, N3 = N2. Plus N1, N2 -> N3 :- N1 = s(N), N3 = Plus(N, s(N2)).", Prolog_Program),
+	transpile_to_prolog("Inc(N) = s(N). Plus(N1, N2) = (* N1 <- 0, N2) Plus(N1, N2) = (* N1 <- s(N), Plus(N, s(N2)))", Prolog_Program),
 	compile_prolog(Prolog_Program),
 	N0 = 0,
-	fun_Inc(N0, N1),
-	fun_Plus(N1, N1, N2),
-	fun_Plus(N2, N2, s(s(s(s(0))))).
+	apply_fun(fun_Inc, N0, N1),
+	apply_fun(fun_Plus, N1, N1, N2),
+	apply_fun(fun_Plus, N2, N2, s(s(s(s(0))))).
+
+test(church) :-
+	transpile_to_prolog("N0(_, X) = X. Inc(N, F, X) = F(N(F, X)). Plus(N, M, F, X) = M(F, N(F, X)).", Prolog_Program),
+	compile_prolog(Prolog_Program).
 
 :- end_tests(transpiler).
