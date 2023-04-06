8430793d1fb366976e1ccdcf7bf66087ab2b6577
OriRoth <ori.rothh@gmail.com>
2023-04-06 22:01:45 +0300

    trits test

--- src/transpiler_test.pl
+++ src/transpiler_test.pl
@@ -2,7 +2,7 @@
 :- use_module(transpiler).
 
 compile_prolog(Prolog_Program) :-
-	string_concat(":- multifile apply_fun/2.\n:- multifile apply_fun/3.\n:- multifile apply_fun/4.\n", Prolog_Program, Prolog_Program1),
+	string_concat(":- multifile apply_fun/2.\n:- multifile apply_fun/3.\n:- multifile apply_fun/4.\n:- multifile apply_fun/5.\n", Prolog_Program, Prolog_Program1),
 	tmp_file_stream(text, File, Stream),
 	write(Stream, Prolog_Program1),
 	close(Stream),
@@ -28,61 +28,44 @@ test(church_numerals) :-
 	compile_prolog(Prolog_Program).
 
 test(church_booleans) :-
-	transpile_to_prolog("T(X, _) :- X. F(_, Y) :- Y. Not(A) :- A(F, T). Or(A, B) :- A(T, B). And(A, B) :- A(B, F).", Prolog_Program),
+	transpile_to_prolog("T(X, _) :- X. F(_, Y) :- Y. NOT(A) :- A(F, T). OR(A, B) :- A(T, B). AND(A, B) :- A(B, F).", Prolog_Program),
 	compile_prolog(Prolog_Program),
 	fun_T(1, 2, 1),
 	fun_F(1, 2, 2),
-	fun_Not(fun_T, fun_F),
-	fun_And(fun_T, fun_T, fun_T),
-	fun_And(fun_T, fun_F, fun_F),
-	fun_Or(fun_T, fun_F, fun_T),
-	fun_Or(fun_F, fun_F, fun_F),
+	fun_NOT(fun_T, fun_F),
+	fun_AND(fun_T, fun_T, fun_T),
+	fun_AND(fun_T, fun_F, fun_F),
+	fun_OR(fun_T, fun_F, fun_T),
+	fun_OR(fun_F, fun_F, fun_F),
 	!.
 
 test(trits) :-
-  atomic_list_concat([
-    "True(X, _,_) :- X." ,
-    "False(_, X,_) :- X. ",
-    "Error(_,_,X) :- X. ",
-    "Not(Trit) :- Trit(False, True,Error).",
-    "Or(A, B) :- A(True, B, Error). ",
-    "And(A, B) :- A(B, False, Error)." ,
-    "Twice(X) :- cons(False,X).",
-    "Increase(X) :- cons(True,X).",
-    "Zero(X) :- cons(Error,Error).",
-    "Zero_0(X) :- Twice(Zero(X)).",
-    "Zero_1(X) :- Twice(Zero_0(X)).",
-    "Zero_2(X) :- Twice(Zero_1(X)).",
-    "Zero_3(X) :- Twice(Zero_2(X)).",
-    "Zero_4(X) :- Twice(Zero_3(X)).",
-    "One(X) :- cons(True,Zero(X)).",
-    "Two(X) :- Twice(One(X)).",
-    "Three(X) :- Increase(One(X)).",
-    "Four(X) :- Twice(Two(X)).",
-    "Five(X) :- Increase(Two(X)).",
-    "Increment(List) :-",
-        {
-          (Head, Rest) = List
-        },
-          Head(
-            (False, Increment(Rest)),    
-            (True,Rest),
-            One
-          ).
-      ]
-    )
-
-  transpile_to_prolog(
-
-Prolog_Program),
+	atomic_list_concat([
+		"True(X, _, _) :- X. ",
+		"False(_, X, _) :- X. ",
+		"Error(_, _, X) :- X. ",
+		"Not(Trit) :- Trit(False, True, Error). ",
+		"Or(A, B) :- A(True, B, Error). ",
+		"And(A, B) :- A(B, False, Error). " ,
+		"Twice(X) :- (False, X). ",
+		"Zero(_) :- (False, (Error, Error)). ",
+		"Increment(List) :-",
+		"	{",
+		"		(Head, Rest) = List",
+		"	},",
+		"	Head(",
+		"		(False, Increment(Rest)),",
+		"		(True, Rest),",
+		"		(Error, Error)",
+		"	)."
+	], DSL_Program),
+	transpile_to_prolog(DSL_Program, Prolog_Program),
 	compile_prolog(Prolog_Program),
-	fun_T(1, 2, 1),
-	fun_F(1, 2, 2),
-	fun_Not(fun_T, fun_F),
-	fun_And(fun_T, fun_T, fun_T),
-	fun_And(fun_T, fun_F, fun_F),
-	fun_Or(fun_T, fun_F, fun_T),
-	fun_Or(fun_F, fun_F, fun_F),
+	fun_Zero(_, Zero),
+	Zero = Zero,
+	%fun_Increment(Zero, One),
+	%fun_Increment(One, Two),
+	%fun_Twice(One, Two),
 	!.
 
 :
