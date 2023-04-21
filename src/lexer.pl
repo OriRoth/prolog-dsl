5dd489c3998fc68a86e01ee970cc2dfb78a84ab3
Yossi Gil <yogi@cs.technion.ac.il>
2023-04-21 14:34:57 +0300

    sync

--- src/lexer.pl
+++ src/lexer.pl
@@ -13,72 +13,75 @@ tokens([])          -->  skip.
 tokens([T])         -->  skip,  token(T).     
 tokens([T1,T2|Ts])  -->  skip,  pair(T1,T2),  tokens(Ts).
 
+token(T)         -->  identifier(T)    |  literal(T)    |  punctuation(T).
+punctuation(T)   -->  ligature(T),!    |  unit(T).         
+literal(L)       -->  number(L)        |  string(L).       
+identifier(I)    -->  alphanumeric(I)  |  symbolic(I).     
+alphanumeric(I)  -->  variable(I)      |  functor(I)    |  tupletor(I).
 
-token(T)       -->  literal(T)  |  identifier(T).     
-literal(L)     -->  number(L)   |  string(L).         
-identifier(I)  -->  functor(I)  |  variable(I)     |  tupletor(I).
 
-
+ligature(L) --> ':
 comment  -->  '/*',!,sequence(Contents),'*/',  {str_type(Contents,ascii),  \+occurs('*/',Contents)}
 /**/     |    '%',sequence(_),newline.                                     
-skip     -->  ''                                                           
-/**/     |    (white|comment),skip.                                        
+skip     -->  (white|comment),skip                                         
+/**      or   */                               |                           ''.
 
 white    -->  space     |  newline   |  blank  |  tab.
-space    -->  '\x20' .                             
-tab      -->  '\t' .                               
-blank    -->  '\f'      |  '\v'.                  
+space    -->  '\x20' .
+tab      -->  '\t' .
+blank    -->  '\f'      |  '\v'.
 newline  -->  '\r\n',!  |  '\n\r',!  |  '\n'   |  '\r'.
 
-arithmetical  -->  '+'   |  '-'  |  '*'   |  '/'.                      
-comparisonal  -->  '<'   |  '>'  |  '='.                               
-punctualtion  -->  ':'   |  '.'  |  ','   |  '?'   |  ';'.             
+arithmetical  -->  '+'   |  '-'  |  '*'   |  '/'.
+comparisonal  -->  '<'   |  '>'  |  '='.
+punctualtion  -->  ':'   |  '.'  |  ','   |  '?'   |  '|'.
 programming   -->  '^'   |  '‘'  |  '~'   |  '@'   |  '#'   |  '$'  |  '&'.
-backslash     -->  '\\'  .                                             
+backslash     -->  '\\'  .
 
 % Operators *
-operator(operator("("))   -->  ['('].  
-operator(operator(")"))   -->  [')'].  
-operator(operator("{"))   -->  ['{'].  
-operator(operator("}"))   -->  ['}'].  
+operator(operator("("))   -->  ['('].
+operator(operator(")"))   -->  [')'].
+operator(operator("{"))   -->  ['{'].
+operator(operator("}"))   -->  ['}'].
 operator(operator(":-"))  -->  [':'],  ['-'].
-operator(operator("."))   -->  ['.'].  
-operator(operator(","))   -->  [','].  
-operator(operator("="))   -->  ['='].  
+operator(operator("."))   -->  ['.'].
+operator(operator(","))   -->  [','].
+operator(operator("="))   -->  ['='].
 
-/***************
- * Identifiers *
- ***************/
-functor(functor(Id))    -->  lower(First),!,    rest(Rest),  {string_chars(Id,[First|Rest])}.
-variable(variable(Id))  -->  upper(First),!,    rest(Rest),  {string_chars(Id,[First|Rest])}.
-tupletor(tupletor(Id))  -->  special(First),!,  rest(Rest),  {string_chars(Id,[First|Rest])}.
+% Identifiers *
+functor(functor(Id))    -->  functorStart(First),!,   continue(First,Id).
+variable(variable(Id))  -->  variableStart(First),!,  continue(First,Id).
+tupletor(tupletor(Id))  -->  tupletorStart(First),!,  continue(First,Id).
 
-lower(C)    -->  C,  char_type(C,prolog_atom_start),  !.
-upper(C)    -->  C,  char_type(C,prolog_var_start),   !.
-special(C)  -->  C,  C='#',                           !.
+continue(First,Id) --> {compose(First,Rest,Id)}.
+
+compose(First,Start,Identifier) :- string_chars(Identifier, [First|Rest]).
+
+functorStart(C)   -->  C,  char_type(C,prolog_atom_start),  !.
+variableStart(C)  -->  C,  char_type(C,prolog_var_start),   !.
+tupletorStart(C)  -->  C,  C='#',                           !.
+
+rest(Rest)        -->  C, continue(C),!,Rest=[C,R],  rest(R)                          |   [];
+
+continue(C),
 
 /*************
  * Utilities *
  *************/
 
-sequence(L)          -->  L=[]                     ;  L=[E|Es],[E],   sequence(Es).          
-prefix(Prefix,Text)  :-   append(Prefix,_,Text),!  .                                         
-occurs(Search,Text)  :-   prefix(Search,Text),!    ;  Text=[_,Rest],  occurs(Search,Rest),!  .
+sequence(L)          -->  L=[]                     |  L=[E|Es],[E],   sequence(Es).
+prefix(Prefix,Text)  :-   append(Prefix,_,Text),!  .
+occurs(Search,Text)  :-   prefix(Search,Text),!    |  Text=[_,Rest],  occurs(Search,Rest),!  .
 
 str_type([], _).
 str_type([First | Rest], Type) :-
 	char_type(First, Type),
 	str_type(Rest, Type).
 
-% Skips 
- 
+% Skips
 
-/************
- * Literals *
- ************/
 
 number(number(Number)) --> sequence(Characters), {string_chars(String,Characters),number_string(Number, String)}.
-
 string(string(String)) --> ['"'], sequence(Contents), ['"'],
 	{
 		str_type(Contents, ascii),
@@ -86,5 +89,3 @@ string(string(String)) --> ['"'], sequence(Contents), ['"'],
 		\+ occurs(['\n'], Contents),
 		string_chars(String, Contents)
 	}.
-
-
